5eeeb9c3a229905a13479fe47c75b480aeac4c03
abayet <anthony.bayet@student.unamur.be>
2022-02-10 12:10:48 -0800

    # Moved animations out of main class

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,7 @@
 import Phaser from 'phaser'
 
+import { createCharacterAnims } from '~/animations/PlayerAnimation'
+
 export default class Game extends Phaser.Scene{
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
     private faune!: Phaser.Physics.Arcade.Sprite
@@ -56,51 +58,9 @@ export default class Game extends Phaser.Scene{
         this.faune = this.physics.add.sprite(128, 128, 'faune', 'walk-down-3.png')
         this.faune.body.setSize(this.faune.width * 0.5, this.faune.height * 0.8)
 
-        this.anims.create({
-            key:'faune-idle-down',
-            frames: [{ key: 'faune', frame: 'walk-down-3.png'}]
-        })
-
-        this.anims.create({
-            key: 'faune-idle-up',
-            frames: [{ key: 'faune', frame: 'walk-up-3.png' }]
-        })
-
-        this.anims.create({
-            key: 'faune-idle-side',
-            frames: [{ key: 'faune', frame: 'walk-side-3.png' }]
-        })
-
-        this.anims.create({
-            key:'faune-run-down',
-            frames: this.anims.generateFrameNames('faune', {start:1, end:8, prefix:'run-down-', suffix:'.png'}),
-            repeat:-1,
-            frameRate:15
-        })
+        createCharacterAnims(this.anims)
         
-        this.anims.create({
-            key: 'faune-run-up',
-            frames: this.anims.generateFrameNames('faune', { start: 1, end: 8, prefix: 'run-up-', suffix: '.png' }),
-            repeat: -1,
-            frameRate: 15
-        })
-
-        this.anims.create({
-            key: 'faune-run-side',
-            frames: this.anims.generateFrameNames('faune', { start: 1, end: 8, prefix: 'run-side-', suffix: '.png' }),
-            repeat: -1,
-            frameRate: 15
-        })
-
-        this.anims.create({
-            key: 'faune-faint',
-            frames: this.anims.generateFrameNames('faune', { start: 1, end: 4, prefix: 'faint-', suffix: '.png' }),
-            frameRate: 15
-        })
-
         this.faune.anims.play('faune-run-down')
-
-
 
         // Add walls layer
         const wallLayer = this.createWalls(tile_size, dungeon_size)
