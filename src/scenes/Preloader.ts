f5da3dd65b49341a50786a90a197c75aa9025cb9
agrabsi <aniss.grabsi@gmail.com>
2022-02-11 14:13:09 -0600

    # waiting for explanation on CORS

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -14,8 +14,23 @@ export default class Preloader extends Phaser.Scene {
         this.load.atlas('player', 'characters/fauna.png', 'characters/fauna.json')
         this.load.atlas('monster', 'enemies/lizard.png', 'enemies/lizard.json')
 
+        // Load metrics data
+        /**
+         * For ratings -> A=1 B=2 C=3 D=4 E=5
+         * 
+         * code_smells = nb code smells
+         * sqale_index = code smells technical debt (in minutes)
+         * sqale_rating = code smells rating 
+         * vulnerabilities = nb vulns
+         * security_rating = security rating
+         * security_remediation_effort = security debt / time to fix everything (in minutes)
+         * bugs = nb bugs
+         * reliability_rating = bugs rating
+         * reliability_remediation_effort = bugs debt / time to fix (in minutes)
+         */
+        // this.load.json('metrics', 'https://sonarcloud.io/api/measures/component_tree?component=brave_brave-core&metricKeys=code_smells,sqale_index,sqale_rating,vulnerabilities,security_rating,security_remediation_effort,bugs,reliability_rating,reliability_remediation_effort&ps=500&p=1')
         
-
+        
     }
 
     create() {
