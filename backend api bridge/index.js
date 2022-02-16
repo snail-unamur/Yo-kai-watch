42c51268d241650420e8488a86a73e24f4540481
abayet <anthony.bayet@student.unamur.be>
2022-02-16 12:34:34 -0800

    # Load only once the data per API execution

--- backend api bridge/index.js
+++ backend api bridge/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { getSonarqubeData, makeTree } from './utils.js';
 
 const app = express();
+let tree;
 
 app.use(cors(
     {
@@ -12,11 +13,15 @@ app.use(cors(
 
 app.get('/metrics', async (req, res) => {
     const project = req.query.project;
-    // Get sonarqube data
-    const sqData = await getSonarqubeData(project, 1, []);
+
+    if(!tree){
+        // Get sonarqube data
+        const sqData = await getSonarqubeData(project, 1, []);
+        
+        // Convert to data structure
+        tree = makeTree(sqData);
+    }
     
-    // Convert to data structure
-    const tree = makeTree(sqData);
     res.json(tree);
 });
 
