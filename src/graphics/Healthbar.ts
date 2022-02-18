58818e07793aad5f3994bc134e86c5ce409dce2b
abayet <anthony.bayet@student.unamur.be>
2022-02-17 16:34:27 -0800

    # Monster health bar updates on damage taken

--- src/graphics/Healthbar.ts
+++ src/graphics/Healthbar.ts
@@ -1,8 +1,13 @@
 export default class HealthBar extends Phaser.GameObjects.Graphics {
     private value = 100
 
-    constructor(scene, x, y) {
-        super(scene, { x:-15.75, y:y })
+    private offsetX: number
+    private offsetY: number
+
+    constructor(scene, offsetY) {
+        super(scene)
+        this.offsetX = -15.75
+        this.offsetY = offsetY
 
         scene.add.existing(this)
         this.draw()
@@ -15,6 +20,7 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
         }
 
         this.draw()
+        return this.value
     }
 
     draw() {
@@ -22,17 +28,19 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
 
         // Background
         this.fillStyle(0x202121)
-        this.fillRect(this.x, this.y, 31.5, 5)
+        this.fillRect(this.offsetX, this.offsetY, 31.5, 5)
 
         // Health
         this.fillStyle(0x69ff4f)
-        this.fillRect(this.x, this.y, 30, 3.5)
+        this.fillRect(this.offsetX, this.offsetY, 30*(this.value/100), 3.5)
 
+        /* TODO: color bar based on reamining health points
+        
         if (this.value < 30) {
             this.fillStyle(0xff0000)
         } else {
             this.fillStyle(0x00ff00)
-        }
+        }*/
     }
    
 }
